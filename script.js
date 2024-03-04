8c0024c4c71830d89a5f8b5aa250c9583c0061b7
CodekaUp <CodekaUp@users.noreply.github.com>
2024-03-04 14:54:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//изменение картинки Headphones
+function OnWhiteHeadphones()
+{
+    headphones.style.backgroundImage = "url('image/headphones_white.png')";
+}
+function OnBlackHeadphones()
+{
+    headphones.style.backgroundImage = "url('image/headphones_black.png')"
+}
+
+//изменение картинки Controller
+function OnWhiteController()
+{
+    controller.style.backgroundImage = "url('image/controller_white.png')";
+}
+function OnBlackController()
+{
+    controller.style.backgroundImage = "url('image/controller_black.png')";
+}
